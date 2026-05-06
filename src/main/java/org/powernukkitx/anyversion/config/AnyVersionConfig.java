82a46bc575d65013755a9b6777997c279a63618a
xRookieFight <me@xrookiefight.com>
2026-05-06 18:24:11 +0300

    feat: implement configuration, fix lombok

---
+++ src/main/java/org/powernukkitx/anyversion/config/AnyVersionConfig.java
@@ -0,0 +1,51 @@
+package org.powernukkitx.anyversion.config;
+
+import lombok.Getter;
+import org.powernukkitx.anyversion.AnyVersion;
+
+import cn.nukkit.utils.Config;
+
+import java.util.Map;
+
+public class AnyVersionConfig {
+
+    public static String CONFIG_VERSION = "1.0.0";
+
+    @Getter
+    private final int lowestVersion;
+    @Getter
+    private final String configVersion;
+
+    public AnyVersionConfig() {
+        var plugin = AnyVersion.getPlugin();
+        plugin.saveDefaultConfig();
+
+        String currentVersion = plugin.getConfig().getString("configVersion", "0.0.0");
+
+        if (!currentVersion.equals(CONFIG_VERSION)) {
+            mergeMissingKeys(plugin);
+
+            plugin.getConfig().set("configVersion", CONFIG_VERSION);
+            plugin.saveConfig();
+        }
+
+        this.lowestVersion = plugin.getConfig().getInt("lowestVersion", 618);
+        this.configVersion = plugin.getConfig().getString("configVersion", CONFIG_VERSION);
+    }
+
+    private void mergeMissingKeys(AnyVersion plugin) {
+        try {
+            Config defaultConfig = new Config("config.yml", Config.YAML);
+            Config currentConfig = plugin.getConfig();
+
+            for (Map.Entry<String, Object> entry : defaultConfig.getAll().entrySet()) {
+                if (!currentConfig.exists(entry.getKey())) {
+                    currentConfig.set(entry.getKey(), entry.getValue());
+                }
+            }
+
+        } catch (Exception e) {
+            plugin.getLogger().error("Config merge error: ", e);
+        }
+    }
+}
