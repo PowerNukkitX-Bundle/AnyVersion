df7f8fe8978d2ad5f548560d7a870cff494f77da
xRookieFight <me@xrookiefight.com>
2026-07-01 12:33:10 +0300

    feat: switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    java
+    alias(libs.plugins.shadow)
+}
+
+group = "org.powernukkitx.anyversion"
+version = "2.8.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    mavenLocal()
+    maven("https://repo.powernukkitx.org/releases")
+    mavenCentral()
+    maven("https://www.jitpack.io")
+    maven("https://repo.opencollab.dev/maven-releases")
+    maven("https://repo.opencollab.dev/maven-snapshots")
+}
+
+dependencies {
+    compileOnly(libs.server)
+    compileOnly(libs.bedrock.connection)
+    compileOnly(libs.okaeri.configs.core)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+}
+
+tasks.shadowJar {
+    archiveBaseName = "AnyVersion"
+    archiveClassifier = ""
+    archiveVersion = ""
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
